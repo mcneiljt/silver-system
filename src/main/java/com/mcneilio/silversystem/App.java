0a6108a3a41ec669b6e57f4d8636eea61c5d5ae0
sarloth <sarloth@gmail.com>
2022-02-04 22:02:07 -0500

    init

---
+++ src/main/java/com/mcneilio/silversystem/App.java
@@ -0,0 +1,96 @@
+package com.mcneilio.silversystem;
+
+import com.mcneilio.silversystem.format.Firehose;
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.json.JSONObject;
+
+import java.time.Duration;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_SERVERS"));
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, System.getenv("KAFKA_GROUP_ID"));
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+
+        KafkaConsumer<String,byte[]> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(System.getenv("KAFKA_TOPIC")));
+
+//        HiveConf hiveConf = new HiveConf();
+//        hiveConf.set("hive.metastore.local", "false");
+//
+//        hiveConf.setVar(HiveConf.ConfVars.METASTOREURIS, System.getenv("HIVE_URL"));
+//        hiveConf.set(HiveConf.ConfVars.PREEXECHOOKS.varname, "");
+//        hiveConf.set(HiveConf.ConfVars.POSTEXECHOOKS.varname, "");
+//        hiveConf.set(HiveConf.ConfVars.HIVE_SUPPORT_CONCURRENCY.varname, "false");
+//        HiveMetaStoreClient hiveMetaStoreClient = null;
+//        try {
+//            hiveMetaStoreClient = new HiveMetaStoreClient(hiveConf, null);
+//        } catch (MetaException e) {
+//            e.printStackTrace();
+//        }
+
+        HashMap<String, List<FieldSchema>> schemas = new HashMap<>();
+
+        while (true) {
+            ConsumerRecords<String,byte[]> records = consumer.poll(Duration.ofMillis(
+                    Integer.parseInt(System.getenv("KAFKA_POLL_DURATION_MS"))));
+            for (ConsumerRecord<String,byte[]> record : records) {
+                Firehose f = new Firehose(record.value());
+                String eventName = f.getTopic();
+                if (!schemas.containsKey(eventName)) {
+                    //try to get the schema and save it in the map
+                    //catch hive no such table error and throw away the record; continue
+                }
+                JSONObject msg = new JSONObject(f.getMessage());
+                List<String> cols = new ArrayList<>();
+                reduceSchema(cols, msg);
+            }
+        }
+    }
+
+    private static void reduceSchema(List<String> cols, JSONObject obj) {
+        obj.keys().forEachRemaining(key -> {
+            key = snakeCase(key);
+            if (!checkPredicate(cols, key)) {
+                //throw away the record
+                return;
+            }
+            if (obj.get(key) instanceof JSONObject) {
+                reduceSchema(cols, (JSONObject) obj.get(key), key);
+            }
+        });
+    }
+
+    private static void reduceSchema(List<String> cols, JSONObject obj, String prefix) {
+        obj.keys().forEachRemaining(key -> {
+            String newKey = prefix + "_" + snakeCase(key);
+            if (!checkPredicate(cols, key)) {
+                obj.remove(key);
+                return;
+            }
+            if (obj.get(key) instanceof JSONObject) {
+                reduceSchema(cols, (JSONObject) obj.get(key), newKey);
+            }
+        });
+
+    }
+
+    private static boolean checkPredicate(List<String> cols, String predicate) {
+        //do any of the cols start with predicate?
+        return true;
+    }
+
+    private static String snakeCase(String k) {
+        //snake casing shenanigans
+        return k;
+    }
+}
---
+++ src/main/java/com/mcneilio/silversystem/format/Firehose.java
@@ -0,0 +1,92 @@
+package com.mcneilio.silversystem.format;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+/**
+ * Originally designed by @thecubed who authored this spec
+ This file defines the Firehose format for messages received on the Kafka FIREHOSE topic
+ Firehose message definition
+ ============================
+ +-------+----------+--------+----------------+-----------+--------------+
+ | magic | topicLen | msgLen |     topic      | separator |     msg      |
+ +-------+----------+--------+----------------+-----------+--------------+
+ | 0x01  | uint8    | uint32 | char[topicLen] | 0x02      | char[msgLen] |
+ +-------+----------+--------+----------------+-----------+--------------+
+ Every firehose object begins with the magic char 0x01 (ASCII start-of-heading)
+ Topic length is expressed as a 8-bit unsigned integer (0x00 - 0xFF)
+ Topics are a sequence of characters with length defined previously in topicLen
+ Message length is expressed as a 32-bit unsigned integer
+ The wrapped message is prefixed by a separator (0x02)
+ A wrapped message should contain a single JSON object, with no trailing newline characters.
+ Firehose objects are lightly compatible with streams, however they should usually be wrapped in a container such
+ as a Kafka message, or stored in an array to provide seekability.
+ Thanks for a little help from @IllegalArgument :)
+ */
+
+public class Firehose {
+
+    public Firehose(String topic, String message) {
+        this.topic = topic;
+        this.message = message;
+        byte[] topicBytes = topic.getBytes();
+        byte[] messageBytes = message.getBytes();
+        byte[] prefix = ByteBuffer.allocate(1).put((byte)0x1).array();
+        byte[] topicLen = ByteBuffer.allocate(1).put((byte)topicBytes.length).array();
+        byte[] messageLen = ByteBuffer.allocate(4).putInt(messageBytes.length).array();
+        byte[] separator = ByteBuffer.allocate(1).put((byte)0x2).array();
+
+        ByteBuffer buffer = ByteBuffer.allocate(
+                prefix.length + topicLen.length + messageLen.length +
+                        separator.length + topicBytes.length + messageBytes.length
+        );
+        buffer.put(prefix);
+        buffer.put(topicLen);
+        buffer.put(messageLen);
+        buffer.put(topicBytes);
+        buffer.put(separator);
+        buffer.put(messageBytes);
+
+        this.byteArray = buffer.array();
+    }
+
+    public Firehose(byte[] byteArray) {
+        this.byteArray = byteArray;
+
+
+        int topicLength = byteArray[1] & 0xff;
+        int msgLength;
+
+        if(System.getenv("ENDIAN").equals("little"))
+            msgLength = ((0xFF & byteArray[5]) << 24) | ((0xFF & byteArray[4]) << 16) |
+                    ((0xFF & byteArray[3]) << 8) | (0xFF & byteArray[2]);
+        else
+            msgLength = ((0xFF & byteArray[2]) << 24) | ((0xFF & byteArray[3]) << 16) |
+                    ((0xFF & byteArray[4]) << 8) | (0xFF & byteArray[5]);
+
+        String fullTopic = new String(Arrays.copyOfRange(byteArray, 6, 6 +
+                topicLength));
+
+        // convert to snake case
+        this.topic = fullTopic.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+
+
+        this.message = new String(Arrays.copyOfRange(byteArray, 7 +
+                topicLength, 7+topicLength+msgLength));
+    }
+
+    public byte[] getByteArray() {
+        return byteArray;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    private byte[] byteArray;
+    private String topic, message;
+}
